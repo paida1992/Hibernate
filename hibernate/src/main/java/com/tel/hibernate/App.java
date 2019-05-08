1a6be3305ea45941b450af3ffbb40ffe59cf4b51
chikki <chikki@priyanka.fios-router.home>
2019-05-07 22:48:57 -0400

    Intial Commit

---
+++ hibernate/src/main/java/com/tel/hibernate/App.java
@@ -0,0 +1,27 @@
+package com.tel.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        alien teluso = new alien();
+        teluso.setAid(101);
+        teluso.setAname("priya");
+        teluso.setColor("blue");
+        
+        Configuration con = new Configuration();
+        SessionFactory sf = con.buildSessionFactory();
+        Session session = sf.openSession();
+    //    System.out.println( "Hello World!11" );
+        session.save(teluso);
+    }
+}
